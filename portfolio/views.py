a5446b04d2d8e58a6a5c4fb7663f3dc2224ecfd4
Preetam <android.preetam@gmail.com>
2018-07-09 18:43:08 +0530

    form submitted with recaptcha

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,11 +1,26 @@
+from django.contrib import messages
 from django.shortcuts import render
 from django.http import Http404
+
+
+# project
+from .forms import MessageForm
 
 # Create your views here.
 
 
 def home(request):
-    if request.method == "GET":
-        return render(request, 'portfolio/home.html')
+    scroll_to_bottom = False
+    if request.method == 'POST':
+        # Form was submitted
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            form = MessageForm()
+            messages.success(request, 'Submission successful')
+        else:
+            scroll_to_bottom = True
+    else:
+        form = MessageForm()
 
-    return Http404('Error Page Not Found!')
+    return render(request, 'portfolio/home.html', {'form': form, 'scroll_to_bottom': scroll_to_bottom})
