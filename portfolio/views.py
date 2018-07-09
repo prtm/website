f8debf42356be0f838586eae2f1bf75dab957605
Preetam <android.preetam@gmail.com>
2018-07-10 00:08:26 +0530

    scroll to bottom on success

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,26 +1,25 @@
+# core django
 from django.contrib import messages
 from django.shortcuts import render
 from django.http import Http404
-
 
 # project
 from .forms import MessageForm
 
 # Create your views here.
 
-
 def home(request):
-    scroll_to_bottom = False
     if request.method == 'POST':
         # Form was submitted
         form = MessageForm(request.POST)
+        scroll_to_bottom = True
         if form.is_valid():
             # Form fields passed validation
+            print(form)
             form = MessageForm()
             messages.success(request, 'Submission successful')
-        else:
-            scroll_to_bottom = True
     else:
+        scroll_to_bottom = False
         form = MessageForm()
 
     return render(request, 'portfolio/home.html', {'form': form, 'scroll_to_bottom': scroll_to_bottom})
