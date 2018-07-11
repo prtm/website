0fd979e8e7b8f1fca4f34b599027d87d35e5fb91
Preetam <android.preetam@gmail.com>
2018-07-11 16:02:48 +0530

    message admin modified

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Message
 # Register your models here.
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'message','created','modified')
+    list_filter = ('created', 'modified')
+    search_fields = ('name','email','message')
+    ordering = ('modified',)
