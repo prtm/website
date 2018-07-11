99c3afeef01813e1981ffe552ccbb59a9115cd0b
Preetam <android.preetam@gmail.com>
2018-07-11 16:09:29 +0530

    desc ordering

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -7,4 +7,4 @@ class MessageAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'message','created','modified')
     list_filter = ('created', 'modified')
     search_fields = ('name','email','message')
-    ordering = ('modified',)
+    ordering = ('-modified','-created')
